d0c7703e4597339f598efeba8f19ca220e454d95
Fábián Gábor <info@fabiangabor.com>
2021-03-02 16:16:54 +0100

    Init

--- Programozasi-kornyezetek-04-Beadando/Program.cs
+++ Programozasi-kornyezetek-04-Beadando/Program.cs
@@ -1,9 +1,6 @@
-﻿namespace Programozasi_kornyezetek_04_Beadando
-{
-  internal class Program
-  {
-    public static void Main(string[] args)
-    {
+﻿namespace Programozasi_kornyezetek_04_Beadando {
+    internal class Program {
+        public static void Main(string[] args) {
+        }
     }
-  }
 }
